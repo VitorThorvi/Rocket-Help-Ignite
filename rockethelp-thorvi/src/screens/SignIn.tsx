8c8f0e5b447279f18c8202d9d036f754bd12fa3a
Vitor Thorvi <euvitoreu@gmail.com>
2022-07-19 16:17:55 -0300

    implement useState

--- rockethelp-thorvi/src/screens/SignIn.tsx
+++ rockethelp-thorvi/src/screens/SignIn.tsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 import {VStack, Heading, Icon, useTheme} from "native-base";
 
 import {Envelope, Key} from 'phosphor-react-native';
@@ -7,12 +9,16 @@ import {Input} from "../components/Input";
 import {Button} from "../components/Button";
 
 export function SignIn() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
     const {colors } = useTheme();
 
     return(
     //    vai retornar o que vai ser exibido na tela
     //    Só pode retornar uma única View
-        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}
+        >
             <Logo />
             {/**/}
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
@@ -23,12 +29,14 @@ export function SignIn() {
                 mb={4}
                 placeholder="E-mail"
                 InputLeftElement={<Icon ml={4} as={<Envelope color={colors.gray[300]}/>} />}
-                />
+                onChangeText={setName}
+            />
             <Input
                 mb={8}
                 placeholder="Senha"
                 InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} /> }
                 secureTextEntry
+                onChangeText={setPassword}
             />
 
             <Button title="Entrar" w="full" />
