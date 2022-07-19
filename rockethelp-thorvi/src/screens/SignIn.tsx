873b552a74a9839b8f17a267a46a5422607b65a1
Vitor Thorvi <euvitoreu@gmail.com>
2022-07-19 16:02:40 -0300

    sign-in layout as in figma

--- rockethelp-thorvi/src/screens/SignIn.tsx
+++ rockethelp-thorvi/src/screens/SignIn.tsx
@@ -1,18 +1,38 @@
-import {VStack, Heading} from "native-base";
+import {VStack, Heading, Icon, useTheme} from "native-base";
+
+import {Envelope, Key} from 'phosphor-react-native';
 
 import Logo from "../assets/logo_primary.svg";
-
-
-
+import {Input} from "../components/Input";
+import {Button} from "../components/Button";
 
 export function SignIn() {
+    const {colors } = useTheme();
+
     return(
     //    vai retornar o que vai ser exibido na tela
     //    Só pode retornar uma única View
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            <Logo />
+            {/**/}
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
                 Acesse sua conta
             </Heading>
+
+            <Input
+                mb={4}
+                placeholder="E-mail"
+                InputLeftElement={<Icon ml={4} as={<Envelope color={colors.gray[300]}/>} />}
+                />
+            <Input
+                mb={8}
+                placeholder="Senha"
+                InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} /> }
+                secureTextEntry
+            />
+
+            <Button title="Entrar" w="full" />
+
         </VStack>
     )
 }
