18bcfa5fc1cd259524c79e572958c5fc25c4deaf
Vitor Thorvi <euvitoreu@gmail.com>
2022-07-19 12:44:57 -0300

    modified view on emulator

---
+++ rockethelp-thorvi/src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native";
+
+export function SignIn() {
+    return(
+    //    vai retornar o que vai ser exibido na tela
+        <View>
+            <Text>Olá, Thorvi</Text>
+        </View>
+    )
+}
